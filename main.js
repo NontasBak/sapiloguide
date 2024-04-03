b2d630045523ded7ca4bebf5241e30122ca3c261
NontasBak <nontasbak@gmail.com>
2024-04-03 21:36:00 +0300

    Implement resizing of all exported images

--- main.js
+++ main.js
@@ -2,40 +2,60 @@ import { tldrawToImage } from "@kitschpatrol/tldraw-cli";
 import Jimp from "jimp";
 import fs from "fs";
 import fsExtra from "fs-extra";
+import path from "path";
 import { ImagesToPDF } from "images-pdf";
 
-const lessons = fs.readdirSync("./src/");
-console.log(lessons);
+async function build() {
+    const lessons = await fs.promises.readdir("./src/");
 
-fsExtra.emptyDirSync("./exported_images");
+    for await (let lesson of lessons) {
+        await fs.promises.mkdir(`./exported_images/${lesson}`);
+        await fs.promises.mkdir(`./resized_images/${lesson}`);
 
-lessons.forEach((lesson) => {
-    fs.mkdirSync(`./exported_images/${lesson}`);
+        const exams = await fs.promises.readdir(`./src/${lesson}/`);
 
-    const exams = fs.readdirSync(`./src/${lesson}/`);
+        for await (let exam of exams) {
+            await fs.promises.mkdir(`./exported_images/${lesson}/${exam}`);
+            await fs.promises.mkdir(`./resized_images/${lesson}/${exam}`);
 
-    exams.forEach((exam) => {
-        fs.mkdirSync(`./exported_images/${lesson}/${exam}`);
-
-        const examSolutions = fs.readdirSync(`./src/${lesson}/${exam}/`);
-
-        examSolutions.forEach((examSolution) => {
-            const imagePath = tldrawToImage(
-                `./src/${lesson}/${exam}/${examSolution}`,
-                {
-                    format: "png",
-                    output: `./exported_images/${lesson}/${exam}/`,
-                    dark: true,
-                }
+            const examSolutions = await fs.promises.readdir(
+                `./src/${lesson}/${exam}/`
             );
-        });
-    });
-});
 
-// Jimp.read("./exported_images/test.png").then((image) => {
-//     image
-//         .scaleToFit(1000, Jimp.AUTO, Jimp.RESIZE_BEZIER)
-//         .write("./resized_images/test_resized.png");
-// });
+            for await (let examSolution of examSolutions) {
+                const imagePath = await tldrawToImage(
+                    `./src/${lesson}/${exam}/${examSolution}`,
+                    {
+                        format: "png",
+                        output: `./exported_images/${lesson}/${exam}/`,
+                        dark: true,
+                    }
+                );
+
+                Jimp.read(
+                    `./exported_images/${lesson}/${exam}/${
+                        path.parse(examSolution).name
+                    }.png`
+                ).then((image) => {
+                    image
+                        .scaleToFit(1000, Jimp.AUTO, Jimp.RESIZE_BEZIER)
+                        .write(
+                            `./resized_images/${lesson}/${exam}/${
+                                path.parse(examSolution).name
+                            }.png`
+                        );
+                });
+
+                console.log(`Finished ${exam}/${examSolution}`);
+            }
+        }
+    }
+}
+
+//clear old images
+fsExtra.emptyDirSync("./exported_images");
+fsExtra.emptyDirSync("./resized_images");
+
+build();
 
 // new ImagesToPDF().convertFolderToPDF("./resized_images", "./pdfs/output.pdf");
